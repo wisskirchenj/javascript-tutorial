5e946464d2dec3b5650a93288ac336083b69578c
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2022-06-18 17:15:44 +0200

    stage 06 completed

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -1,4 +1,11 @@
-// select modal-btn,modal-overlay,close-btn
-// listen for click events on modal-btn and close-btn
-// when user clicks modal-btn add .open-modal to modal-overlay
-// when user clicks close-btn remove .open-modal from modal-overlay
+const modalBtn = document.querySelector(".modal-btn");
+const overlay = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+modalBtn.addEventListener('click', () =>{
+  overlay.classList.add('open-modal');
+});
+
+closeBtn.addEventListener('click', () =>{
+  overlay.classList.remove('open-modal');
+});
--- README.md
+++ README.md
@@ -17,6 +17,8 @@
 16.06.22: Project 05 completed 'Sidebar' -> use \<aside> for sidebar with fixed width and use classlist.toggle and classlist.remove on button listeners.
 Extensive use of fontawesome - and animations with @keystroke
 
+18.06.22: Project 06 completed 'Modal' -> again using classList.add /.remove, this time to toggle visibility and z-index. Glasspane by transparency 0.5 overlay..
+
 ## Reference: All projects in action here
 
 [Projects](https://www.vanillajavascriptprojects.com/)
