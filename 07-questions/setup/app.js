72aba89e1f4fdc523a787fef7e8053def24894c1
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2022-06-18 18:28:06 +0200

    working stage 7 on my own...

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,17 @@
-//using selectors inside the element
-// traversing the dom
+const expandBtns = document.querySelectorAll('.question-btn.plus-icon');
+const collapseBtns = document.querySelectorAll('.question-btn.minus-icon');
+
+
+expandBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.add('show-text');
+  });
+});
+
+collapseBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.remove('show-text');
+  });
+});
