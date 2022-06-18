d1831bb60b835cb8ac9c0157c3a010d1f38d4798
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2022-06-18 18:35:29 +0200

    close other texts on new open

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,17 +1,23 @@
 const expandBtns = document.querySelectorAll('.question-btn.plus-icon');
 const collapseBtns = document.querySelectorAll('.question-btn.minus-icon');
-
+const questions = document.querySelectorAll('.question');
 
 expandBtns.forEach((btn) => {
   btn.addEventListener('click', (e) => {
     const question = e.currentTarget.parentElement.parentElement;
+    removeAllShowTexts();
     question.classList.add('show-text');
   });
 });
 
 collapseBtns.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    const question = e.currentTarget.parentElement.parentElement;
-    question.classList.remove('show-text');
+  btn.addEventListener('click', () => {
+    removeAllShowTexts();
   });
 });
+
+function removeAllShowTexts() {
+  questions.forEach((question) => {
+    question.classList.remove('show-text');
+  });
+}
