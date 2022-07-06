89fa49b0ef9cce5d1604b798a90cfe1a039003aa
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2022-07-06 23:32:50 +0200

    scroll events - fixed width

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -8,8 +8,48 @@ window.addEventListener('DOMContentLoaded', () => {
   document.querySelector(".date").textContent = new Date().getFullYear();
 });
 // ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+
+navToggle.addEventListener('click', () => {
+  //linksContainer.classList.toggle("show-links");
+
+  /* works but not nice
+  let items = links.childElementCount;
+  if (linksContainer.style.height.startsWith("0")) {
+    linksContainer.style.height = (50 * items) + "px";
+  } else {
+    linksContainer.style.height = 0;
+  } */
+  let linksHeight = links.getBoundingClientRect().height;
+  let containerHeight = linksContainer.getBoundingClientRect().height;
+
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
 
 // ********** fixed navbar ************
+const navbar = document.getElementById("nav");
+const topBtn = document.querySelector(".top-link");
+window.addEventListener('scroll', () => {
+  const navHeight = navbar.getBoundingClientRect().height;
+  const offset = window.pageYOffset;
+  if (offset > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+  if (offset > 200) {
+    topBtn.classList.add("show-link");
+  } else {
+    topBtn.classList.remove("show-link");
+  }
+});
 
 // ********** smooth scroll ************
 // select links
--- README.md
+++ README.md
@@ -25,7 +25,7 @@ Extensive use of fontawesome - and animations with @keystroke
 
 04.07.22: Project 09 completed 'Video' -> video muted/autoplay, video play() and pause(), preload - window-'load' event
 
-xx.07.22: Project 10 completed 'Scroll' -> scroll-behavior: smooth, overflow: hidden,
+xx.07.22: Project 10 completed 'Scroll' -> scroll-behavior: smooth, overflow: hidden, Element.getBoundingClientRect(), new Date(), scrollEvent listening, window.pageYoffset read-only, !important in css overwrites element style attributes inline.
 
 ## Reference: All projects in action here
 
