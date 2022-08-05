109cbe30e947ddd175a97f89ca1f4d90c7b95cc0
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2022-08-05 13:13:02 +0200

    fully functional (except reaching expire time) version of stage 12

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,55 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+const expContainer = document.querySelector("#expDate");
+
+let expDate = new Date();
+expDate.setSeconds(expDate.getSeconds() + 35);
+
+const expires = weekdays[expDate.getDay()] + ", "
+  + expDate.getDate() + " " + months[expDate.getMonth()]
+  + " " + expDate.getFullYear() + " "
+  + timeAmerican(expDate);
+
+function timeAmerican(expDate) {
+  let hours = expDate.getHours();
+  let amPm = "am";
+  if (hours >= 12) {
+    amPm = "pm";
+    if (hours > 12) {
+      hours -= 12;
+    }
+  } else {
+    if (hours === 0) {
+      hours = 12;
+    }
+  }
+  return zeroPad(hours) + ":"
+  + zeroPad(expDate.getMinutes()) + amPm;
+}
+
+expContainer.textContent = expires;
+
+// countdown
+
+const daysVal = document.querySelector("#days");
+const hoursVal = document.querySelector("#hours");
+const minsVal = document.querySelector("#mins");
+const secsVal = document.querySelector("#secs");
+setCountDownValues(); // initial call
+
+setInterval(() => {
+  setCountDownValues();
+}, 1000);
+
+function setCountDownValues() {
+  let timeDiff = expDate.getTime() - new Date().getTime();
+  daysVal.textContent = zeroPad(timeDiff / (1000 * 60 * 60 * 24));
+  hoursVal.textContent = zeroPad((timeDiff / (1000 * 60 * 60)) % 24);
+  minsVal.textContent = zeroPad((timeDiff / (1000 * 60)) % 60);
+  secsVal.textContent = zeroPad((timeDiff / (1000)) % 60);
+}
+
+function zeroPad(aFloat) {
+  return Math.trunc(aFloat).toString().padStart(2, '0');
+}
