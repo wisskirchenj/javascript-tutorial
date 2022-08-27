4685c20e63d3d329e54af64103376d87794f8e12
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2022-08-27 18:27:43 +0200

    project 15 completed no circling

---
+++ 15-slider/setup/app.js
@@ -0,0 +1,33 @@
+const prevBtn = document.querySelector(".prevBtn");
+const nextBtn = document.querySelector(".nextBtn");
+const slides = document.querySelectorAll(".slide");
+
+document.addEventListener('DOMContentLoaded', () => {
+  prevBtn.style.display = 'none';
+});
+const slideCount = slides.length;
+let currentSlide = 1;
+
+nextBtn.addEventListener('click', slideNext);
+prevBtn.addEventListener('click', slidePrev);
+
+function slideNext() {
+  prevBtn.style.display = '';
+  if (++currentSlide === slideCount) {
+    nextBtn.style.display = 'none';
+  }
+  translateScreen();
+}
+
+function slidePrev() {
+  nextBtn.style.display = '';
+  if (--currentSlide === 1) {
+    prevBtn.style.display = 'none';
+  }
+  translateScreen();
+}
+
+function translateScreen() {
+  let transform = 100 * (currentSlide - 1);
+  slides.forEach(item => item.style.transform = `translateX(-${transform}%)`);
+}
