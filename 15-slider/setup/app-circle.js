1c9f2bd56ce6d4c2a191feded82be19e16c03db2
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2022-08-27 18:39:08 +0200

    project 15 - including circling

---
+++ 15-slider/setup/app-circle.js
@@ -0,0 +1,24 @@
+const prevBtn = document.querySelector(".prevBtn");
+const nextBtn = document.querySelector(".nextBtn");
+const slides = document.querySelectorAll(".slide");
+
+const slideCount = slides.length;
+let currentSlide = 1;
+
+nextBtn.addEventListener('click', slideNext);
+prevBtn.addEventListener('click', slidePrev);
+
+function slideNext() {
+  currentSlide = currentSlide % slideCount + 1;
+  translateScreen();
+}
+
+function slidePrev() {
+  currentSlide = (currentSlide + slideCount - 2) % slideCount + 1;
+  translateScreen();
+}
+
+function translateScreen() {
+  let transform = 100 * (currentSlide - 1);
+  slides.forEach(item => item.style.transform = `translateX(-${transform}%)`);
+}
